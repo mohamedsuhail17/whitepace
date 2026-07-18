967e7bc730c580ffb973fcee97265eb80ae06bba
Mohamed Suhail J <mdsuhail5055@gmail.com>
2026-07-18 11:06:29 +1100

    add Header setup tailwindcss

---
+++ src/components/firstpage/Header.jsx
@@ -0,0 +1,16 @@
+import '/src/App.css'
+import logo from "/src/assets/Logo.png"
+import menuBtn from "/src/assets/Btn.png"
+export default function Header(){
+    return(
+        <header className="flex bg-bg-header justify-between p-4">
+            <div className="logo">
+                <img src={logo} alt="comapny logo" />
+            </div>
+           
+            <div>
+                <img src={menuBtn} alt="comapny logo" />
+            </div>
+        </header>
+    )
+}
