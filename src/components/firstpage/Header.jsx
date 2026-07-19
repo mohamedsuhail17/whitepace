aafad0d263d12730da1c2d9d51e6d3ac9f7a3b5f
Mohamed Suhail J <mdsuhail5055@gmail.com>
2026-07-19 18:49:34 +1100

    add 2xl responsiveness

--- src/components/firstpage/Header.jsx
+++ src/components/firstpage/Header.jsx
@@ -5,24 +5,42 @@ import rightArrow from "/src/assets/Group 212.png"
 
 export default function Header(){
     return(
-        <header className="flex justify-between items-center p-4 mb-10 2xl:justify-end gap-6">
+        <header className="flex justify-between items-center p-4 md:p-6 xl:p-8 mb-10 xl:justify-end gap-6 4xl:px-52">
             
-            <div className="logo 2xl:mr-auto">
+            <div className="logo xl:mr-auto">
                 <img src={logo} alt="comapny logo" />
             </div>
+            <div className="hidden 2xl:block text-white 2xl:flex 2xl:gap-15 mr-10">
+                <select name="" id="">
+                    <option value="product" default>Product</option>
+                    <option value=""></option>
+                </select>
+                <select name="" id="">
+                    <option value="solution" default>Solution</option>
+                    <option value=""></option>
+                </select>
+                <select name="" id="">
+                    <option value="resources" default>Resources</option>
+                    <option value=""></option>
+                </select>
+                <select name="" id="">
+                    <option value="pricing" default>Pricing</option>
+                    <option value=""></option>
+                </select>
+            </div>
 
-            <div className="hidden 2xl:block w-32 font-bold h-16 bg-btn-login px-10 py-4 rounded-lg ">
+            <div className="hidden xl:block w-32 font-bold h-16 bg-btn-login px-10 py-4 rounded-lg ">
                 <button>Login</button>
             </div>
 
-            <div className="hidden 2xl:block  w-55 h-16 text-white bg-btn-free-trial rounded-lg items-center justify-center px-6 py-4 2xl:flex 2xl:gap-4">
+            <div className="hidden xl:block  w-55 h-16 text-white bg-btn-free-trial rounded-lg items-center justify-center px-6 py-4 xl:flex xl:gap-4">
                  <span>Try Whitepace Free</span> 
                  <div className="w-[10px] h-[10px]">
                      <img className="w-full h-full" src={rightArrow} alt="arrow" />
                  </div>
             </div>
            
-            <div>
+            <div className="2xl:hidden">
                 <img src={menuBtn} alt="comapny logo" />
             </div>
         </header>
--- src/components/firstpage/Main.jsx
+++ src/components/firstpage/Main.jsx
@@ -2,25 +2,22 @@ import rightArrow from "/src/assets/Group 212.png"
 
 export default function Main(){
     return(
-        <main className="grid mx-4 mb-15 2xl:grid-cols-2">
-            <div className="2xl:flex 2xl:flex-col 2xl:items-start">
-                <h4 className="h-22 w-72 text-header4 px-4 mx-auto mb-6 font-bold text-white 2xl:text-4xl 2xl:text-start 2xl:px-0 2xl:mx-0 2xl:py-2">
+        <main className="grid mx-4 md:mx-6 xl:mx-8 mb-15 xl:grid-cols-2 xl:mt-30 4xl:mx-52">
+            <div className="xl:flex xl:flex-col xl:items-start xl:gap-11 2xl:py-10 4xl:mb-">
+                <h4 className="h-22 w-72 text-header4 px-4 mx-auto mb-6 font-bold text-white xl:text-6xl xl:text-start xl:px-0 xl:mx-0 xl:py-2 xl:w-fit xl:mb-8">
                     Get More Done with whitepace
                 </h4>
-                <p className="h-30 text-center text-lg text-white mb-15 2xl:text-start">
+                <p className="h-30 text-center text-lg text-white mb-15 xl:mb-0 xl:text-start">
                     Project management software that enables your teams to collaborate, plan, analyze and manage everyday tasks
                 </p>
-
-                <div className="mx-auto py-8 w-58 h-18 text-white bg-btn-free-trial rounded-lg mb-25 flex items-center justify-center gap-4 2xl:mb-0 2xl:mx-0">
+                <div className="mx-auto py-8 w-58 h-15 text-white bg-btn-free-trial rounded-lg mb-25 flex items-center justify-center gap-4 xl:mb-0 xl:mx-0">
                     <span>Try TasKey Free</span> 
                     <div className="w-[10px] h-[10px]">
                         <img className="w-full h-full" src={rightArrow} alt="arrow" />
                     </div>
                 </div>
             </div>
-            
-            <div className="bg-image-container h-48 w-full text-white 2xl:h-full">
-            
+            <div className="bg-image-container h-50 max-w-7/10 w-full mx-auto text-white xl:h-full xl:max-w-full">
             </div>
         </main>
     )
