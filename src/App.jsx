37c155bca908f8dd7b4bfe4fbf0a991084ede725
Mohamed Suhail J <mdsuhail5055@gmail.com>
2026-07-24 09:26:47 +1100

    add background image

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import FirstPage from "./components/firstpage/FirstPage";
 import SecondPage from "./components/secondpage/SecondPage";
 import ThirdPage from "./components/thirdpage/ThirdPage";
 import FourthPage from "./components/fourthpage/FourthPage";
+import FifthPage from "./components/fifthpage/FifthPage";
 import SixthPage from "./components/sixthpage/SixthPage";
 import SeventhPage from "./components/seventhpage/SeventhPage";
 import EighthPage from "./components/eighthpage/EighthPage";
 import NinthPage from "./components/ninthpage/NinthPage";
+import TenthPage from "./components/tenthpage/TenthPage";
 import EleventhPage from "./components/eleventhpage/EleventhPage";
 import Footer from "./components/footer/Footer";
-import FifthPage from "./components/fifthpage/FifthPage";
 
 
 export default function App(){
@@ -26,6 +27,7 @@ export default function App(){
       <SeventhPage />
       <EighthPage />
       <NinthPage />
+      <TenthPage />
       <EleventhPage />
       <Footer />
     </BrowserRouter>
--- src/components/sixthpage/SixthPage.jsx
+++ src/components/sixthpage/SixthPage.jsx
@@ -1,16 +1,20 @@
 import Button from "../button/Button";
+import sixthImg from "/src/assets/page6/page-6.png"
+import fourXlImg from "/src/assets/page6/4xl-page-6.png"
 
 export default function SixthPage(){
 
     return (
-        <section className="sixth-page bg-first-page">
+        <section className="sixth-page bg-first-page relative z-1">
+            <img className="absolute -z-3 opacity-25 hidden 4xl:block 4xl:-top-44" src={fourXlImg} alt="background image for 4xl" />
+            <img className="absolute -z-3 opacity-25 hidden md:block xl:hidden 2xl:block md:h-full 2xl:-top-12 2xl:h-[40vw] 2xl:w-[20vw] 2xl:-left-18 4xl:hidden" src={sixthImg} alt="background image upto 2xl" />
             <article className="px-4 pt-25 pb-35 md:px-5 md:py-35 xl:px-8 4xl:px-55">
                 <div className="flex flex-col gap-15 items-start">
-                    <div className="text-block text-left 2xl:text-center">
-                        <h4 className="text-4xl font-bold font-inter text-white mb-6">
+                    <div className="text-block text-left 2xl:text-center font-inter">
+                        <h4 className="heading-1 text-white">
                             Your work, everywhere you are
                         </h4>
-                        <p className="leading[1.875rem] text-white">
+                        <p className="description-1 text-white">
                             Access your notes from your computer, phone or tablet by synchronising with various services, including whitepace, Dropbox and OneDrive. The app is available on Windows, macOS, Linux, Android and iOS. A terminal app is also available!
                         </p>
                     </div>
